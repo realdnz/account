2125e219ca4319efa597880996e87af5951286dd
realdnz <16971073@qq.com>
2019-10-11 22:16:14 +0800

    第一次提交

---
+++ Hotel/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Hotel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public JsonResult ajaxtest()
+        { JsonResult result = new JsonResult();
+            result.Data = new { name = "DNZ", AGE = 29 };
+            result.JsonRequestBehavior =JsonRequestBehavior.AllowGet;
+            return result;
+
+        }
+    }
+}
